e330d9056b7e0a6bd61242869367b0affa486776
Boyz2705 <rifky.mahdiyah.ferrari@gmail.com>
2024-09-24 17:53:12 +0700

    user favicon

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="icon" type="image/x-icon" href="/assets/sibook_fav.png">
     <title>Login</title>
 
     <script src="https://code.iconify.design/2/2.2.1/iconify.min.js"></script>
--- resources/views/homeassets.blade.php
+++ resources/views/homeassets.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/x-icon" href="/assets/sibook_fav.png">
     <title>Sibook</title>
     <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
     <link rel="stylesheet" href="{{ asset('css/cssku.css') }}">
